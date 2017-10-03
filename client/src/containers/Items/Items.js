1a106a5aaeb83965cb389ae89b61e8c8d95f84cb
Mahreen Qureshi <mahreenq@hotmail.com>
2017-10-03 16:57:43 -0400

    testing items.js

--- client/src/containers/Items/Items.js
+++ client/src/containers/Items/Items.js
@@ -3,10 +3,39 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
 
 const Items = () => {
   return (
-    <p> Testing </p>
+    <Card>
+
+    <CardMedia
+      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+    >
+      <img src="https://i.pinimg.com/736x/f4/30/48/f430483aa680e61f927e7f78cf158d6a--roller-quad-roller-disco.jpg" alt="" />
+
+
+
+    </CardMedia>
+
+    <CardHeader
+      title="URL Avatar"
+      subtitle="Subtitle"
+      avatar="http://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg"
+    />
+    <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+    <CardActions>
+      <FlatButton label="Borrow" />
+    </CardActions>
+  </Card>
 
   );
 }
