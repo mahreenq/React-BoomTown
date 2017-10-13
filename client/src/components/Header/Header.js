cecda1bf60fea77301f4b45e66b09cc1ce3a2ea8
Mahreen Qureshi <mahreenq@hotmail.com>
2017-10-13 09:23:12 -0400

    Branch using React only

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -15,6 +15,37 @@ import MenuItem from 'material-ui/MenuItem';
 import logo from '../../images/boomtown-logo.svg';
 
 
+
+
+// const Dropdown = () => {
+  
+//     return (
+//       <div className="LeftNav">
+//     {/* <Link to = '/' >  <img className="boomlogo" src={logo} /> </Link> */}
+    
+//      <img className="boomlogo" src={logo} /> 
+//       <SelectField style={{marginLeft:`1.5rem`}}
+//           floatingLabelText="Filter by Tag"
+         
+//           /* value={this.state.value}
+//           onChange={this.handleChange} */
+//           >
+//         <MenuItem value={1} primaryText="Household Items" />
+//         <MenuItem value={2} primaryText="Recreational Equipment" />
+//         <MenuItem value={3} primaryText="Musical Instruments" />
+//         <MenuItem value={4} primaryText="Tools" />
+//         <MenuItem value={5} primaryText="Physical Media" />
+//         <MenuItem value={6} primaryText="Sporting Goods" />
+//         <MenuItem value={7} primaryText="Electronics" />
+//       </SelectField >
+
+//   </div>
+  
+//       );
+//   }
+
+
+
 const Dropdown = (
   <div className="LeftNav">
     {/* <Link to = '/' >  <img className="boomlogo" src={logo} /> </Link> */}
